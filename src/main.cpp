e02ff2d76a9968b5d96e2a44afb8bdc43145cfd6
8-Lambda-8 <j.wasle111@gmail.com>
2024-04-28 21:07:55 +0200

    request json

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
 
 #include "wifiPasswd.h"
 
+WiFiClientSecure client;
+
+#define HOST "api.awattar.at"
+
+void makeHTTPRequest();
 
 void setup() {
   Serial.begin(115200);
@@ -23,6 +30,70 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  client.setInsecure();
+
+  makeHTTPRequest();
+}
+
+void makeHTTPRequest() {
+  // Opening connection to server (Use 80 as port if HTTP)
+  if (!client.connect(HOST, 443)) {
+    Serial.println(F("Connection failed"));
+    return;
+  }
+
+  // give the esp a breather
+  yield();
+
+  // Send HTTP request
+  client.print(F("GET "));
+  client.print("/v1/marketdata");
+  client.println(F(" HTTP/1.1"));
+
+  // Headers
+  client.print(F("Host: "));
+  client.println(HOST);
+
+  client.println(F("Cache-Control: no-cache"));
+
+  if (client.println() == 0) {
+    Serial.println(F("Failed to send request"));
+    return;
+  }
+
+  //  Check HTTP status
+  char status[32] = {0};
+  client.readBytesUntil('\r', status, sizeof(status));
+  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+    Serial.print(F("Unexpected response: "));
+    Serial.println(status);
+    return;
+  }
+
+  // Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!client.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    return;
+  }
+
+  // peek() will look at the character, but not take it off the queue
+  while (client.available() && client.peek() != '{') {
+    char c = 0;
+    client.readBytes(&c, 1);
+    Serial.print(c);
+    Serial.println("BAD");
+  }
+  JsonDocument doc;
+
+  DeserializationError error = deserializeJson(doc, client);
+
+  if (!error) {
+  } else {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
 }
 
 void loop() {
